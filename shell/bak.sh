3763cddb81455c62233f2acff91a908fe8a55cfc
sowevo <i@sowevo.com>
2021-07-04 00:51:52 +0800

    修改配置

--- shell/bak.sh
+++ shell/bak.sh
@@ -5,7 +5,7 @@ echo "开始导出..."
 mysqldump -h192.168.5.248 -P3306 -udigitalthread -pdigitalthread --databases digitalthread_objectbuilder digitalthread_basicconfig digitalthread_cat digitalthread_microservice digitalthread_system >bak.sql
 # 导入
 echo "开始导入..."
-mysql -h192.168.21.230 -P3306 -uroot -proot < bak.sql
+mysql -hdev.nancal.co -P3306 -uroot -phsgz8bz < bak.sql
 # 删除sql文件
 echo "删除中间文件..."
 rm -f bak.sql
