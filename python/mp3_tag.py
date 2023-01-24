5a3e8178fb83c6982ad660ca6fdb3f9174a52d85
sowevo <i@sowevo.com>
2023-01-24 14:33:04 +0800

    处理下载的评书mp3文件的tag信息

--- python/mp3_tag.py
+++ python/mp3_tag.py
@@ -23,23 +23,30 @@ for filename in os.listdir(music_path):
     if filename.endswith(".mp3"):
         file_path = os.path.join(music_path, filename)
         audiofile = eyed3.load(file_path)
-        if audiofile.tag:
-            match = re.search(regular, filename)
-            if match:
-                episode = int(match.group(1))
-                print("文件名:", filename)
-                print("曲目编号:", audiofile.tag.track_num)
-                print("集数：", episode)
-                print("标题:", title % episode)
-                print("作者:", artist)
-                print("专辑:", album)
-                print("==================")
+        if not audiofile.tag:
+            audiofile.initTag()
+        match = re.search(regular, filename)
+        if match:
+            episode = int(match.group(1))
+            print("文件名:", filename)
+            print("集数：", episode)
+            print("标题:", title % episode)
+            print("作者:", artist)
+            print("专辑:", album)
+            print("==================")
 
-                audiofile.tag.artist = artist
-                audiofile.tag.album = album
-                audiofile.tag.title = title % episode
-                audiofile.tag.track_num = episode
-                audiofile.tag.images.set(ImageFrame.FRONT_COVER, open(cover_path, 'rb').read(), "image/jpeg")
-                audiofile.tag.save(encoding='utf-8')
-            else:
-                raise ValueError("无法从%s中获取集数信息" % filename)
+            audiofile.tag.track_num = None
+            audiofile.tag.artist = artist
+            audiofile.tag.album = album
+            audiofile.tag.title = title % episode
+            audiofile.tag.track_num = episode
+            audiofile.tag.images.set(ImageFrame.FRONT_COVER, open(cover_path, 'rb').read(), "image/jpeg")
+            try:
+                # 删除注释...
+                del audiofile.tag.frame_set[eyed3.id3.frames.COMMENT_FID]
+            except:
+                pass
+            # 保存
+            audiofile.tag.save(encoding='utf-8', version=(2, 3, 0))
+        else:
+            raise ValueError("无法从%s中获取集数信息" % filename)
