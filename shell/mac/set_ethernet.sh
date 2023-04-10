fe4e00f653fa011f8d62747ce6abc8ca25c19814
sowevo <i@sowevo.com>
2023-04-10 13:05:37 +0800

    .

--- shell/mac/set_ethernet.sh
+++ shell/mac/set_ethernet.sh
@@ -6,9 +6,9 @@ if [[ $# -ne 1 || ($1 != "auto" && $1 != "manual") ]]; then
 fi
 
 if [[ $1 == "auto" ]]; then
-    networksetup -setdhcp Ethernet
-    echo "Ethernet network is set to DHCP."
+    networksetup -setdhcp Wi-Fi
+    echo "Wi-Fi network is set to DHCP."
 else
-    networksetup -setmanual Ethernet 192.168.21.119 255.255.255.0 192.168.21.100
+    networksetup -setmanual Wi-Fi 192.168.21.119 255.255.255.0 192.168.21.254
     echo "Ethernet network is set to manual mode."
 fi
