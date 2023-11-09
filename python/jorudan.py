0a232c9c7308c1abe41547e827464d9b961dc37a
sowevo <i@sowevo.com>
2023-11-09 16:51:29 +0800

    shinkansen time

--- python/jorudan.py
+++ python/jorudan.py
@@ -4,6 +4,7 @@ import requests_cache
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 from retrying import retry
 import re
+import sqlite3
 import json
 from tqdm import tqdm
 
@@ -76,25 +77,25 @@ def get_schedule_info(_schedule_url):
     eki_elements = soup.find_all("tr", class_="js_rosenEki")
     for eki in eki_elements:
         name = eki.find("td", class_="eki").text.strip()
-        time = eki.find("td", class_="time").text.strip().replace(" 発", "")
+        time = eki.find("td", class_="time").text.strip().replace(" 発", "").replace(" 着", "")
         info_url = eki.find("a", class_="noprint")["href"]
 
         station_info.append({
-            'Id': params['lid'][0],
+            'Id': int(params['lid'][0]),
             'Name': info['Name'],
             'Number': info['Number'],
             'Series': info['Series'],
             'Direction': info['Direction'],
             "StopName": name,
             "Time": time,
-            "StopInfoURL": base_url+info_url
+            "StopInfoURL": base_url + info_url
         })
     return station_info
 
 
 def extract_information(text):
     # 使用正则表达式来匹配信息，允许车型信息可选，并排除括号
-    pattern = r'^(.*?)\d+号(?:\((.*?)系\))? *\((.*?)行\)の運行表$'
+    pattern = r'^(.*?)\d+号(?:\((.*?)\))? *\((.*?)行\)の運行表$'
     match = re.match(pattern, text)
 
     if match:
@@ -107,7 +108,6 @@ def extract_information(text):
         series = None
         direction = None
         number = None
-
     return {
         'Name': name,
         'Number': number,
@@ -116,7 +116,47 @@ def extract_information(text):
     }
 
 
+# Function to create SQLite table
+def create_schedule_table():
+    conn = sqlite3.connect('schedule_info.sqlite')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS schedule_info (
+            Id TEXT,
+            Name TEXT,
+            Number TEXT,
+            Series TEXT,
+            Direction TEXT,
+            StopName TEXT,
+            Time TEXT,
+            StopInfoURL TEXT
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+
+# Function to insert data into SQLite table
+def insert_schedule_data(schedule_info):
+    conn = sqlite3.connect('schedule_info.sqlite')
+    cursor = conn.cursor()
+
+    for info in schedule_info:
+        cursor.execute('''
+            INSERT INTO schedule_info VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (
+            info['Id'], info['Name'], info['Number'], info['Series'], info['Direction'], info['StopName'], info['Time'],
+            info['StopInfoURL']))
+
+    conn.commit()
+    conn.close()
+
+
 if __name__ == '__main__':
+    # 创建 SQLite 表
+    create_schedule_table()
     station_urls = get_station_url()
     schedule_urls_set = set()
     for station_url in tqdm(station_urls, desc="加载站点信息...", ncols=100):
@@ -125,10 +165,12 @@ if __name__ == '__main__':
     schedule_urls = sorted(schedule_urls_set)
     schedule_infos = []
     for schedule_url in tqdm(schedule_urls, desc="加载班次信息...", ncols=100):
+        infos = get_schedule_info(schedule_url)
+        # 将 schedule_infos 写入 SQLite 文件
+        insert_schedule_data(infos)
         for info in get_schedule_info(schedule_url):
             schedule_infos.append(info)
 
     # 将 schedule_infos 写入 JSON 文件
     with open('schedule_infos.json', 'w', encoding='utf-8') as json_file:
         json.dump(schedule_infos, json_file, ensure_ascii=False, indent=4)
-
