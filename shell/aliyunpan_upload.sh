856496f5008a6643603e6351a28430429b0451d1
sowevo <i@sowevo.com>
2024-01-25 21:39:21 +0800

    .

--- shell/aliyunpan_upload.sh
+++ shell/aliyunpan_upload.sh
@@ -10,14 +10,19 @@
 #   sh -c "$(fetch -o - https://raw.githubusercontent.com/Sowevo/my_script/main/shell/aliyunpan_upload.sh)"
 #   sh -c "$(fetch -o - https://ghproxy.com/https://raw.githubusercontent.com/Sowevo/my_script/main/shell/aliyunpan_upload.sh)"
 
-echo -e "请输入要处理的文件路径"
-read -r -p "文件路径: " path
+echo -e "请输入要处理的本地文件夹路径"
+read -r -p "本地文件夹路径: " path
 # 判断他是一个存在的文件夹
 if [[ ! -d "$path" ]]; then
   echo "文件夹不存在"
   exit 1
 fi
-# 打印一下这个路径下的所有文件夹,只要第一级
-find $path -maxdepth 1 -mindepth 1 -type d -print0 | sort -z | while IFS= read -r -d '' file; do printf './aliyunpan upload -exn "DS_Store" -exn "\.jpg$" -exn "\.nfo$" -exn "\.png$" "%s"/ /media_volume/series/  \n' "$file"; done
+echo -e "请输入要上传的目标位置"
+read -r -p "目标位置: " target
+
+# 打印出来本地文件夹下的所有第一级文件夹
+# 然后拍个序
+# 然后拼接成阿里云盘上传的命令
+find $path -maxdepth 1 -mindepth 1 -type d -print0 | sort -z | while IFS= read -r -d '' file; do printf './aliyunpan upload -exn "DS_Store" -exn "\.jpg$" -exn "\.nfo$" -exn "\.png$" "%s"/ '"${target}"'  \n' "$file"; done
 
 
